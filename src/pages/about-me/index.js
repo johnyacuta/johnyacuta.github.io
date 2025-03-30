85f6de725bd8a450f30e34e9e633c5ea473774cf
johnyacuta <11905221+johnyacuta@users.noreply.github.com>
2025-03-30 16:46:37 -0700

    Fix ext

--- src/pages/about-me/index.js
+++ src/pages/about-me/index.js
@@ -15,7 +15,7 @@ class AboutMe extends Component {
         </Row>
         <Row>
           <Col>
-            <Image src="/john-about-me.jpg" rounded style={{marginTop: 50, marginBottom: 50, width: '25rem'}}></Image>
+            <Image src="/john-about-me.JPG" rounded style={{marginTop: 50, marginBottom: 50, width: '25rem'}}></Image>
           </Col>
           <Col style={{marginTop: 50, marginBottom: 50, width: '25rem'}}>
             <p class="text-left">
